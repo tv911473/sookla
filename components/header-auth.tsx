89b5e75ca00a7296f1350e6ba06b5bc23eab1186
Taavi <144431054+tv911473@users.noreply.github.com>
2024-11-04 16:35:43 +0200

    feat:remove un-needed page components

--- components/header-auth.tsx
+++ components/header-auth.tsx
@@ -1,7 +1,6 @@
 import { signOutAction } from "@/app/actions";
 import { hasEnvVars } from "@/utils/supabase/check-env-vars";
 import Link from "next/link";
-import { Badge } from "./ui/badge";
 import { Button } from "./ui/button";
 import { createClient } from "@/utils/supabase/server";
 
@@ -16,14 +15,6 @@ export default async function AuthButton() {
     return (
       <>
         <div className="flex gap-4 items-center">
-          <div>
-            <Badge
-              variant={"default"}
-              className="font-normal pointer-events-none"
-            >
-              Please update .env.local file with anon key and url
-            </Badge>
-          </div>
           <div className="flex gap-2">
             <Button
               asChild
