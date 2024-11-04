d309520c143bd7f80b5c631b7b90381101703ac3
Ester Ojala <146342718+esteroja@users.noreply.github.com>
2024-11-04 17:16:49 +0200

    feat: create recipe feed route

---
+++ app/recipe-feed/page.tsx
@@ -0,0 +1,10 @@
+
+export default async function Index() {
+  return (
+    <>
+      <main className="flex-1 flex flex-col gap-6 px-4">
+        <h2 className="font-medium text-xl mb-4"></h2>
+      </main>
+    </>
+  );
+}
