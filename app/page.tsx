bf13b371991442181bd01a6a18c5cee738684f6c
Taavi <144431054+tv911473@users.noreply.github.com>
2024-11-07 13:41:51 +0200

    feat: hero page edit, add pulsing sign-up button

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,9 @@ export default async function Index() {
     <>
       <Hero />
       <main className="flex-1 flex flex-col gap-6 px-4">
-        <h2 className="font-medium text-xl mb-4">PEALEHT</h2>
+        <div className="flex flex-col gap-2 items-center">
+          <h2 className="font-medium text-xl mb-4">PEALEHT</h2>
+        </div>
       </main>
     </>
   );
