7ca49d0f4a2a077e0848713401f2ddb9d934bee1
Ester Ojala <146342718+esteroja@users.noreply.github.com>
2024-11-07 14:13:50 +0200

    fix: Recipe feed display compact design

--- components/recipes/RecipeCard.tsx
+++ components/recipes/RecipeCard.tsx
@@ -1,9 +1,10 @@
 import { Recipe } from "@/types/Recipe";
 
 export function RecipeCard({ recipe }: { recipe: Recipe }) {
-  const placeholderImage = "https://via.placeholder.com/150"; // Placeholder image URL
-  
+  const placeholderImage = "https://via.placeholder.com/150";
+
   return (
+    
     <li key={recipe.id} className="p-4 mb-4 border rounded-lg shadow-md">
       <img
         src={placeholderImage}
@@ -12,25 +13,19 @@ export function RecipeCard({ recipe }: { recipe: Recipe }) {
       />
       <h1 className="text-xl font-semibold">{recipe.title}</h1>
       <br />
-      <span className="block text-sm text-gray-700">Portsjonid: {recipe.servings}</span>
-      <br />
-      <span className="block text-sm text-gray-700">Kategooria: {recipe.categories.category_name}</span>
-      <br />
-      <span className="block text-sm text-gray-700">Valmistusaeg: {recipe.total_time_minutes} minutit</span>
-      <br />
-      <span className="block text-sm text-gray-700">
-        Koostisosad:
-        <ol className="list-decimal pl-5">
-          {recipe.ingredients.ingredient_text
-            .split(",")
-            .map((ingredient: string) => (
-              <li className="text-sm text-gray-600">{ingredient.trim()}</li>
-            ))}
-        </ol>
-      </span>
-      <br />
-      <span className="block text-sm text-gray-700">Valmistusjuhend: </span>
-      <span className="block text-sm text-gray-700">{recipe.steps_description}</span>
+      <div className="flex flex-wrap flex-col">
+        <div className="flex text-sm text-gray-700">
+          Portsjonid: {recipe.servings}
+        </div>
+        <br />
+        <div className="flex text-sm text-gray-700">
+          Kategooria: {recipe.categories.category_name}
+        </div>
+        <br />
+        <div className="flex text-sm text-gray-700">
+          Valmistusaeg: {recipe.total_time_minutes} minutit
+        </div>
+      </div>
     </li>
   );
 }
