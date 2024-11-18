01f75d25be0409708d59520ba1afc5a44cb915fe
Taavi <144431054+tv911473@users.noreply.github.com>
2024-11-18 15:44:33 +0200

    feat: add like button

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,21 @@
 import Hero from "@/components/hero";
+import RecipeFeed from "./recipe-feed/page";
+import { getAllRecipesAction } from "./actions";
 
 export default async function Index() {
+  const allRecipes = await getAllRecipesAction();
+
   return (
     <>
       <Hero />
       <main className="flex-1 flex flex-col gap-6 px-4">
         <div className="flex flex-col gap-2 items-center">
           <h2 className="font-medium text-xl mb-4">PEALEHT</h2>
+        </div>
+        <div className="flex-1 w-full flex flex-col gap-12">
+          <div className="flex flex-col gap-2 items-start">
+            <RecipeFeed recipes={allRecipes} isLoggedIn={false} />
+          </div>
         </div>
       </main>
     </>
--- app/recipe-feed/page.tsx
+++ app/recipe-feed/page.tsx
@@ -3,14 +3,15 @@ import { Recipe } from "@/types/Recipe";
 
 interface RecipeFeedProps {
   recipes: Recipe[];
+  isLoggedIn: boolean;
 }
 
-export default function RecipeFeed({ recipes }: RecipeFeedProps) {
+export default function RecipeFeed({ recipes, isLoggedIn }: RecipeFeedProps) {
   return (
     <div className="px-4 py-6">
       <ul className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
         {recipes.map((recipe: Recipe) => (
-          <RecipeCard key={recipe.id} recipe={recipe} />
+          <RecipeCard key={recipe.id} recipe={recipe} isLoggedIn={isLoggedIn} />
         ))}
       </ul>
     </div>
