052dc9059cb982e74524879cc96d68d766ca7d34
Jörgen Kristofer Rebane <jkrebane@gmail.com>
2024-11-21 13:11:40 +0200

    feat: add search by UID functionality

--- app/protected/user-profile/[id]/page.tsx
+++ app/protected/user-profile/[id]/page.tsx
@@ -1,30 +1,29 @@
 import { createClient } from "@/utils/supabase/server";
+import { notFound } from "next/navigation";
+import BackButton from "@/components/back-button";
 
 export default async function UserProfilePage({
   params,
 }: {
   params: { id: string };
 }) {
-  const { id } = await params; // id laheb labi URLi, saab panna ka ilmselt mingi otsingu ja search asja lehele endale
+  const { id } = await params;
+
   const supabase = await createClient();
 
   // kasutaja otsimine id jargi
-  const { data: user, error } = await supabase
-    .from("users") // public.users supabase
+  const { data, error } = await supabase
+    .from("users")
     .select("*")
-    .eq("id", id) // id = await params koht
+    .eq("id", id)
     .single();
 
-  console.log("Küsi kasutajat ID-ga:", id);
-
-  if (!user) {
-    return <p>Kasutajat ei leitud või toimus viga.</p>;
+  if (error || !data) {
+    notFound();
   }
 
   // Hetkel pole tabelis created_at fieldi seega ehk hakkab toole kui lisada
-  const formattedDate = new Date(user.created_at).toLocaleDateString("en-GB");
-
-  const username = user.username || "Kasutajanimi pole määratud";
+  const formattedDate = new Date(data.created_at).toLocaleDateString("en-GB");
 
   return (
     <div className="flex flex-col items-center p-6 max-w-lg mx-auto bg-white rounded-xl shadow-md space-y-6">
@@ -36,26 +35,30 @@ export default async function UserProfilePage({
           className="w-full h-full object-cover rounded-full"
         />
       </div>
-
-      {/* Kasutajanimi */}
+      {/* Kasutaja andmed */}
       <div className="w-full bg-gray-100 p-4 rounded-lg shadow-inner space-y-4">
         <div>
           <p className="text-sm text-gray-500">Kasutajanimi:</p>
-          <p className="text-lg font-medium">{username}</p>
+          <p className="text-lg font-medium">
+            {data.username || "Kasutajanimi pole määratud"}
+          </p>
         </div>
         <div>
           <p className="text-sm text-gray-500">E-mail:</p>
-          <p className="text-lg font-medium">{user.email}</p>
+          <p className="text-lg font-medium">{data.email}</p>
         </div>
         <div>
           <p className="text-sm text-gray-500">Kasutaja ID:</p>
-          <p className="text-lg font-medium">{user.id}</p>
+          <p className="text-lg font-medium">{data.id}</p>
         </div>
         <div>
           <p className="text-sm text-gray-500">Liitumiskuupäev:</p>
           <p className="text-lg font-medium">{formattedDate}</p>
         </div>
       </div>
+
+      {/* Tagasi nupp */}
+      <BackButton />
     </div>
   );
 }
