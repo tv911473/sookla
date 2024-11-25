1666dcf07c54063375a082acf152d3f5234d0f00
Jörgen Kristofer Rebane <jkrebane@gmail.com>
2024-11-25 16:05:52 +0200

    feat: add navigation to followers profiles when clicked on username

--- components/followers-list.tsx
+++ components/followers-list.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { useState } from "react";
+import { useRouter } from "next/navigation";
 
 type FollowerData = {
   follower_id: string;
@@ -15,12 +16,18 @@ interface FollowersListProps {
 
 const FollowersList = ({ followers }: FollowersListProps) => {
   const [isFollowersVisible, setIsFollowersVisible] = useState(false);
+  const router = useRouter();
 
   const toggleFollowers = () => {
     setIsFollowersVisible((prev) => !prev);
   };
 
   const hasFollowers = followers && followers.length > 0;
+
+  // navigeerib followeri lehele
+  const handleClickUser = (id: string) => {
+    router.push(`/protected/user-profile/${id}`);
+  };
 
   return (
     <div className="w-full bg-gray-100 p-4 rounded-lg shadow-inner mt-6">
@@ -45,7 +52,11 @@ const FollowersList = ({ followers }: FollowersListProps) => {
       {isFollowersVisible && hasFollowers && (
         <ul className="space-y-2 mt-4">
           {followers.map((follower) => (
-            <li key={follower.follower_id} className="text-sm font-medium">
+            <li
+              key={follower.follower_id}
+              className="text-sm font-medium cursor-pointer text-blue-500 hover:underline"
+              onClick={() => handleClickUser(follower.follower_id)}
+            >
               {follower.users?.username || "Anonüümne kasutaja"}
             </li>
           ))}
