e33a1f103addf90da0caa2ef004d233a1e577dbd
Taavi <144431054+tv911473@users.noreply.github.com>
2024-11-25 16:47:41 +0200

    feat: add "lisa retsept" when user don't have recipes

--- app/protected/user-recipes/page.tsx
+++ app/protected/user-recipes/page.tsx
@@ -2,6 +2,7 @@ import { createClient } from "@/utils/supabase/server";
 import { redirect } from "next/navigation";
 import RecipeFeed from "../../recipe-feed/page";
 import { getUserRecipesAction } from "@/app/actions";
+import Link from "next/link"; // Import Link from Next.js
 
 export default async function ProtectedPage() {
   const supabase = await createClient();
@@ -17,13 +18,25 @@ export default async function ProtectedPage() {
   const userRecipes = await getUserRecipesAction(user.id);
 
   return (
-    <div className="flex-1 w-full flex flex-col gap-12">
+    <div className="flex-1 w-full flex flex-col gap-12 min-h-screen">
       <div className="flex flex-col gap-2 items-start">
-        <RecipeFeed
-          recipes={userRecipes}
-          isLoggedIn={false}
-          isUserPage={true}
-        />
+        {userRecipes.length === 0 ? (
+          <div className="text-xl font-semibold">
+            <p>Retseptid puudu</p>
+            <Link
+              href="/protected/recipe-form"
+              className="text-blue-500 hover:text-blue-700 underline"
+            >
+              Lisa retsept
+            </Link>
+          </div>
+        ) : (
+          <RecipeFeed
+            recipes={userRecipes}
+            isLoggedIn={false}
+            isUserPage={true}
+          />
+        )}
       </div>
     </div>
   );
