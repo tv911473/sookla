098888729365a9186be0a43bdfd2a35db2f010a7
Taavi <144431054+tv911473@users.noreply.github.com>
2024-11-28 17:15:42 +0200

    fix: center text
    forgot password page

--- app/(auth-pages)/forgot-password/page.tsx
+++ app/(auth-pages)/forgot-password/page.tsx
@@ -12,7 +12,7 @@ export default async function ForgotPassword(props: {
   const searchParams = await props.searchParams;
   return (
     <>
-      <form className="flex-1 flex flex-col w-full gap-2 text-foreground [&>input]:mb-6 min-w-64 max-w-64 mx-auto">
+      <form className="flex-1 flex flex-col w-full gap-2 text-foreground [&>input]:mb-6 min-w-64 max-w-64 mx-auto items-center m-10">
         <div>
           <h1 className="text-2xl font-medium">Parooli taastamine</h1>
           <p className="text-sm text-secondary-foreground">
