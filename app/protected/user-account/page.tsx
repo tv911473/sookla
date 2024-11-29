2760482619bb5a0448a2f1a2eec2093ea04743bf
Jörgen Kristofer Rebane <jkrebane@gmail.com>
2024-11-29 13:43:54 +0200

    style: add footer spacing

--- app/protected/user-account/page.tsx
+++ app/protected/user-account/page.tsx
@@ -13,7 +13,6 @@ export default async function UserAccountPage() {
     return <p>Sa pead olema sisse logitud et näha seda lehte.</p>;
   }
 
-  // username saamise query
   const { data: userData, error } = await supabase
     .from("users")
     .select("username")
@@ -24,43 +23,45 @@ export default async function UserAccountPage() {
     return <p>Kasutaja andmeid ei leitud.</p>;
   }
 
-  const formattedDate = new Date(user.created_at).toLocaleDateString("en-GB");
+  //const formattedDate = new Date(user.created_at).toLocaleDateString("en-GB");
   const username = userData.username || "Kasutajanimi pole määratud";
 
   return (
-    <div className="flex flex-col items-center p-6 max-w-md w-full mx-auto bg-white rounded-xl shadow-md space-y-6 m-16">
-      {/* profiilipilt kui tahame lisada siis on koht olemas */}
-      <div className="w-24 h-24 bg-gray-200 rounded-full mb-4">
-        <img
-          src="path/to/profile-picture"
-          alt="Konto Profiilipilt"
-          className="w-full h-full object-cover rounded-full"
+    <div className=" min-h-screen">
+      <div className="flex flex-col items-center p-6 max-w-md w-full mx-auto bg-white rounded-xl shadow-md space-y-6 m-16">
+        {/* profiilipilt kui tahame lisada siis on koht olemas */}
+        <div className="w-24 h-24 bg-gray-200 rounded-full mb-4">
+          <img
+            src="path/to/profile-picture"
+            alt="Konto Profiilipilt"
+            className="w-full h-full object-cover rounded-full"
+          />
+        </div>
+
+        {/* Muuda kasutajanime */}
+        <ChangeUsernameButton
+          email={user.email as string}
+          initialUsername={username}
         />
-      </div>
 
-      {/* Muuda kasutajanime */}
-      <ChangeUsernameButton
-        email={user.email as string}
-        initialUsername={username}
-      />
-
-      {/* Konto info */}
-      <div className="w-full bg-gray-100 p-4 rounded-lg shadow-inner space-y-4">
-        <div>
-          <p className="text-sm text-gray-500">Kasutajanimi:</p>
-          <p className="text-lg font-medium">{username}</p>
-        </div>
-        <div>
-          <p className="text-sm text-gray-500">E-mail:</p>
-          <p className="text-lg font-medium">{user.email}</p>
-        </div>
-        <div>
+        {/* Konto info */}
+        <div className="w-full bg-gray-100 p-4 rounded-lg shadow-inner space-y-4">
+          <div>
+            <p className="text-sm text-gray-500">Kasutajanimi:</p>
+            <p className="text-lg font-medium">{username}</p>
+          </div>
+          <div>
+            <p className="text-sm text-gray-500">E-mail:</p>
+            <p className="text-lg font-medium">{user.email}</p>
+          </div>
+          {/* <div>
           <p className="text-sm text-gray-500">Kasutaja alates:</p>
           <p className="text-lg font-medium">{formattedDate}</p>
-        </div>
+        </div> */}
 
-        {/* muuda parooli nupp */}
-        <ChangePasswordButton email={user.email as string} />
+          {/* muuda parooli nupp */}
+          <ChangePasswordButton email={user.email as string} />
+        </div>
       </div>
     </div>
   );
