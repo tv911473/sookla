500228498b28e2f94dbbd0366d6acbc9d0beed7b
Ester Ojala <146342718+esteroja@users.noreply.github.com>
2024-12-03 22:58:48 +0200

    fix: invalid login credentials note

--- components/form-message.tsx
+++ components/form-message.tsx
@@ -12,12 +12,12 @@ export function FormMessage({ message }: { message: Message }) {
         </div>
       )}
       {"error" in message && (
-        <div className="text-destructive-foreground border-l-2 border-destructive-foreground px-4">
+        <div className="text-foreground border-l-2 border-foreground px-4">
           {message.error}
         </div>
       )}
       {"message" in message && (
-        <div className="text-foreground border-l-2 px-4">{message.message}</div>
+        <div className="text-foreground border-l-2 border-foreground px-4">{message.message}</div>
       )}
     </div>
   );
